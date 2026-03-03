e01fbf3743bb3f7339e3ff93587825810d6d09f7
Samarjeet <samarsabale1021@gmail.com>
2026-03-03 12:01:29 +0530

    Refactor HTML structure, update CSS for dark mode functionality, and streamline JavaScript for improved performance.

--- CSS_Button_component_library/script.js
+++ CSS_Button_component_library/script.js
@@ -1,134 +1,28 @@
 
-// Dark and Light Mode Btn :
 
-const modeBtn = document.querySelector('.mode-btn');
-let darkModeStatus = localStorage.getItem("darkModeStatus") === "true";
+// Dark Mode switcher
 
-if (darkModeStatus === true) {
-    darkModeOn();
-}
+const button = document.querySelector(".inner__box");
+const body = document.querySelector("body");
 
-modeBtn.addEventListener('click', () => {
-    darkModeStatus = !darkModeStatus;
-    console.log(darkModeStatus);
+let modeOn = false;
 
-    if (darkModeStatus === true) {
+button.addEventListener('click', ()=> {
+    if (modeOn === false) {
         darkModeOn();
+        body.classList.add('mode__dark');
     } else {
         darkModeOff();
+        body.classList.remove('mode__dark');
     }
-
-    localStorage.setItem("darkModeStatus", darkModeStatus);
 });
 
-
-function darkModeOn () {
-    modeBtn.classList.add('active');
-    document.body.classList.add('dark');
+function darkModeOn() {
+    button.classList.add('dark');
+    modeOn = true;
 }
 
-function darkModeOff () {
-    modeBtn.classList.remove('active');
-    document.body.classList.remove('dark');
-};
-
-
-// Like and Unlike Button :
-
-
-const countLike = document.querySelectorAll('.like-count');
-const likeBtn = document.querySelectorAll('.likeBtn');
-
-likeBtn.forEach((btn, index) => {
-
-
-    let isActive = localStorage.getItem(`likeStatus-${index}`) === "true";
-    let likeCount = parseInt(localStorage.getItem(`likeCount-${index}`)) || 0;
-
-    if (isActive === true) {
-        like(btn, index);
-    }
-
-    countLike[index].textContent = `${likeCount}`;
-
-    btn.addEventListener('click', () => {
-        isActive = !isActive;
-        console.log(isActive);
-
-        if (isActive) {
-            likeCount = like(btn, likeCount);
-        } else {
-            likeCount = dislike(btn, likeCount);
-        }
-
-        countLike[index].textContent = `${likeCount}`;
-
-        //  Save to Local Storage :
-
-        localStorage.setItem(`likeStatus-${index}`, isActive);
-        localStorage.setItem(`likeCount-${index}`, likeCount);
-    });
-
-
-});
-
-function like (btn, likeCount) {
-    btn.classList.add('active');
-    return likeCount =+ 1;
+function darkModeOff() {
+    button.classList.remove('dark');
+    modeOn = false;
 }
-
-function dislike (btn, likeCount) {
-    btn.classList.remove('active');
-    return likeCount -= 1;
-};
-
-
-const tabs = document.querySelectorAll('.tab');
-const all_content = document.querySelectorAll('.code-content .content');
-
-tabs.forEach( (tab, index) => {
-    tab.addEventListener('click', (e) => {
-        tabs.forEach((tab) => {
-            tab.classList.remove('active');
-        })
-        tab.classList.add('active');
-
-        all_content.forEach((content) => {
-            content.classList.remove('active');
-        })
-        all_content[index].classList.add('active');
-    })
-})
-
-
-const closeBtn = document.querySelector('.close-btn');
-const overlay = document.querySelector('.overlay');
-const getCodeBtn = document.querySelectorAll('.codeBtn');
-
-getCodeBtn.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        openOverlay();
-    })
-})
-
-closeBtn.addEventListener('click', () => {
-    closeOverlay();
-})
-
-overlay.addEventListener('click', (e) => {
-    if (e.target === overlay) {
-        closeOverlay();
-    }
-})
-
-
-function openOverlay() {
-    overlay.classList.add('open');
-    document.querySelector('body').classList.add('lock-scroll');
-}
-
-function closeOverlay () {
-    overlay.classList.remove('open');
-    document.querySelector('body').classList.remove('lock-scroll');
-}
-
